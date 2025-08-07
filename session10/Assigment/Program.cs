41350b0c0fd7c5a7ff1188ef1c68109d1afae508
ayaa7744 <ayaashraf72003@gmail.com>
2025-08-08 01:58:24 +0300

    Add files via upload

---
+++ session10/Assigment/Program.cs
@@ -0,0 +1,22 @@
+﻿using BankSystem;
+
+namespace Assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount account1 = new BankAccount();
+            account1.ShowAccountDetails();
+
+            // Object 2: Parameterized constructor
+            BankAccount account2 = new BankAccount("Aya Ashraf", "12345678901234", "01012345678", "Cairo", 5000);
+            account2.ShowAccountDetails();
+
+            // Object 3: Overloaded constructor
+            BankAccount account3 = new BankAccount(" Ali", "98765432109876", "01123456789", "Alexandria");
+            account3.ShowAccountDetails();
+        }
+    }
+}
